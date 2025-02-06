7f07d0a105ab481500d717425269ce06347f2e90
Matheus Moraes <matheusbmoraes0@gmail.com>
2025-02-06 07:40:45 -0300

    função de exibição dos produtos finalizada

---
+++ static/JS/ecommercePage.js
@@ -0,0 +1,40 @@
+async function fetchProducts(){
+    try{
+        let response = await fetch('/static/JS/productsData.json');
+        if(!response.ok){
+            throw new Error('Erro ao carregar os produtos: ' + response.statusText);
+        }
+        const productsList = await response.json();
+
+        return productsList;
+
+    } catch(error){
+        console.error(error);
+
+        return [];
+    }
+}
+
+async function displayProducts(){
+    try{
+        const productsList = await fetchProducts();
+        if(!!productsList.length){
+            let productsHTML = '';
+            productsList.forEach((product, index) => {
+                productsHTML += `
+                    <div class="productCard" id="${index}">
+                        <img class="productImage" src="${product.image}" alt="Imagem do produto">
+                        <h3>${product.title}</h3>
+                        <p>${product.description}</p>
+                        <h4>Preço: R$${product.price}</h4>
+                    </div>
+                `;
+            });
+            document.getElementById('main').innerHTML = productsHTML;
+        }
+    } catch(error){
+        console.error(error);
+    }
+}
+
+window.onload = displayProducts;
